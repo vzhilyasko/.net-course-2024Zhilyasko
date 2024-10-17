996ccd20f1f6928fcd841aad44a967084703d1c2
vzhilyasko <vzhilyasko@gmail.com>
2024-10-17 23:22:10 +0300

    10. Добавлены методы  записи и чтения информации о клиентах банка в файл csv,  а также обновлении информации о клиентах из файла и добавлении новых клиентов банка из файла

---
+++ ExportTool/ExportService.cs
@@ -0,0 +1,109 @@
+﻿using System.Globalization;
+using System.Text;
+using BankSystem.Data.Storages;
+using BankSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExportTool
+{
+    public class ExportService
+    {
+        private string PathToDirecory { get; set; }
+        private string CSVFileName { get; set; }
+
+        public ExportService(string pathToDirectory, string csvFileName)
+        {
+            PathToDirecory = pathToDirectory;
+            CSVFileName = csvFileName;
+        }
+
+        public void WriteClientToCsv(List<Client> clients)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(PathToDirecory);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            string fullPath = Path.Combine(PathToDirecory, CSVFileName);
+
+            using (FileStream fileStream = new FileStream(fullPath, FileMode.OpenOrCreate))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(fileStream, Encoding.UTF8))
+                {
+                    using (var writer = new CsvHelper.CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+                    {
+                        writer.WriteRecords(clients);
+                        writer.Flush();
+                    }
+                }
+            }
+        }
+
+        public List<Client> ReadClientToCsv()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(PathToDirecory);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            string fullPath = Path.Combine(PathToDirecory, CSVFileName);
+
+            using (FileStream fileStream = new FileStream(fullPath, FileMode.Open))
+            {
+                using (StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    using (var reader = new CsvHelper.CsvReader(streamReader, CultureInfo.InvariantCulture))
+                    {
+                        var clients = reader.GetRecords<Client>().ToList();
+                        return clients;
+                    }
+                }
+            }
+        }
+
+        public void UpdateClientsFromFile()
+        {
+            var clients = ReadClientToCsv();
+
+            if (clients == null)
+            {
+                throw new ArgumentNullException("Файл пуст");
+            }
+
+            var context = new BankSystemDbContext();
+            var clientsStorageEF = new ClientStorageEF(context);
+
+            clients.ForEach(x =>
+            {
+                clientsStorageEF.Update(x);
+            });
+        }
+
+        public void AddClientsFromFile()
+        {
+            var clients = ReadClientToCsv();
+
+            if (clients != null)
+            {
+                clients.ForEach(x =>
+                {
+                    x.Id = Guid.Empty;
+                });
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+            
+            var context = new BankSystemDbContext();
+            var clientsStorageEF = new ClientStorageEF(context);
+
+            clients.ForEach(x =>
+            {
+                clientsStorageEF.Add(x);
+            });
+        }
+    }
+}
