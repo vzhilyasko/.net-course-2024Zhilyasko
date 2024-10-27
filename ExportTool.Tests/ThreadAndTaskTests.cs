00c1cd192b4c3ddb5c6029d928e8071d7530c250
vzhilyasko <vzhilyasko@gmail.com>
2024-10-27 11:43:20 +0200

    12. Добавлено начисление денег в аккаунт в потоках

--- ExportTool.Tests/ThreadAndTaskTests.cs
+++ ExportTool.Tests/ThreadAndTaskTests.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using BankSystem.Data.Storages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BankSystem.App.Services;
 
 namespace ExportTool.Tests
 {
-    internal class ThreadAndTaskTests
+    public class ThreadAndTaskTests
     {
+        private TestDataGeneratorServise _dataGenerator = new TestDataGeneratorServise();
+        
+        [Fact]
+        public void AddAmountInAccountsInThreads()
+        {
+            var account = _dataGenerator.GenerateListAccount()[0];
+           
+            var threadAdd1 = new Thread(() =>
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    account.Amount += 100;
+                }
+            });
+
+            var threadAdd2 = new Thread(() =>
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    account.Amount += 100;
+                }
+            });
+
+            threadAdd1.Start();
+            threadAdd2.Start();
+
+            threadAdd1.Join();
+            threadAdd2.Join();
+        }
     }
 }
